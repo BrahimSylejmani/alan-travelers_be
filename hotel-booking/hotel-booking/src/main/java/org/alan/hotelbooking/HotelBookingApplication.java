29249dfea2e7a37d889cca4e6bf2eafc483c09f1
BrahimSylejmani <bs57810@ubt-uni.net>
2024-04-09 16:49:34 +0200

    Set up hotel-booking service

---
+++ hotel-booking/hotel-booking/src/main/java/org/alan/hotelbooking/HotelBookingApplication.java
@@ -0,0 +1,13 @@
+package org.alan.hotelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingApplication.class, args);
+	}
+
+}
