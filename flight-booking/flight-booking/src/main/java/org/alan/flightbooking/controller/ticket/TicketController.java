9d3116cb4f1d5f3221576deb69c35e337c676f97
Altin <altinmorina2002@gmail.com>
2024-04-24 23:22:36 +0200

    Create CRUD operations for tickets -Implement ticket controller - Implement ticket service -Implement ticket repository

---
+++ flight-booking/flight-booking/src/main/java/org/alan/flightbooking/controller/ticket/TicketController.java
@@ -0,0 +1,51 @@
+package org.alan.flightbooking.controller.ticket;
+import org.alan.flightbooking.common.dto.ticket.TicketRequestDTO;
+import org.alan.flightbooking.common.dto.ticket.TicketResponseDTO;
+import org.alan.flightbooking.service.ticket.TicketService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tickets")
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @PostMapping("/{userId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TicketResponseDTO createTicket(@PathVariable Long userId, @Valid @RequestBody TicketRequestDTO ticketRequestDTO) {
+        return ticketService.createTicket(userId, ticketRequestDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TicketResponseDTO>> getAllTickets() {
+        List<TicketResponseDTO> tickets = ticketService.getAllTickets();
+        return ResponseEntity.ok(tickets);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TicketResponseDTO> getTicketById(@PathVariable Long id) {
+        TicketResponseDTO ticket = ticketService.getTicketById(id);
+        return ResponseEntity.ok(ticket);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TicketResponseDTO> updateTicket(@PathVariable Long id, @Valid @RequestBody TicketRequestDTO ticketRequestDTO) {
+        TicketResponseDTO updatedTicket = ticketService.updateTicket(id, ticketRequestDTO);
+        return ResponseEntity.ok(updatedTicket);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTicket(@PathVariable Long id) {
+        ticketService.deleteTicket(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ flight-booking/flight-booking/src/main/java/org/alan/flightbooking/service/ticket/TicketService.java
@@ -0,0 +1,20 @@
+package org.alan.flightbooking.service.ticket;
+
+
+import org.alan.flightbooking.common.dto.ticket.TicketRequestDTO;
+import org.alan.flightbooking.common.dto.ticket.TicketResponseDTO;
+
+import java.util.List;
+
+public interface TicketService {
+
+    TicketResponseDTO createTicket(Long userId, TicketRequestDTO ticketRequestDTO);
+
+    List<TicketResponseDTO> getAllTickets();
+
+    TicketResponseDTO getTicketById(Long id);
+
+    TicketResponseDTO updateTicket(Long id, TicketRequestDTO ticketRequestDTO);
+
+    void deleteTicket(Long id);
+}
